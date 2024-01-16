996a5ff897007cef91f9bf6be5f4d3f618435009
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-01-16 12:40:41 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,4 +5,5 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for software to be ready ..."
 sleep 30s;
 
-docker-compose exec -T api bash -c "node ./packages/api/dist/scripts/bootstrap.js"
+# docker-compose exec -T api bash -c "node ./packages/api/dist/scripts/bootstrap.js"
+docker-compose exec -T admin-cli bash -c './admin.sh bootstrap --workspace-name="Workspace"'
