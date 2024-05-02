aa04f004c8114b749fc339fd79df6b78c600248b
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-05-02 12:02:05 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -7,3 +7,12 @@ sleep 30s;
 
 # docker-compose exec -T api bash -c "node ./packages/api/dist/scripts/bootstrap.js"
 docker-compose exec -T admin-cli bash -c './admin.sh bootstrap --workspace-name="Workspace"'
+
+if [ -e "./initialized" ]; then
+    echo "Already initialized, skipping..."
+else
+    sed -i 's@limit_req_log_level warn;@limit_req_log_level warn;\n    proxy_read_timeout 300;\n    proxy_connect_timeout 300;\n    proxy_send_timeout 300;\n@g' /opt/elestio/nginx/conf.d/${DOMAIN}.conf
+
+    docker exec elestio-nginx nginx -s reload;
+    touch "./initialized"
+fi
